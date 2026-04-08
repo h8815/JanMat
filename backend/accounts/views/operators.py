ef7893a5ff39434bd05404a35f78c012c9fd4c21
h8815 <shimanshu8181@gmail.com>
2026-04-09 03:07:49 +0530

    Fix: email sending error

--- backend/accounts/views/operators.py
+++ backend/accounts/views/operators.py
@@ -1,4 +1,7 @@
 from accounts.constants import SystemRoles
+import os
+from django.conf import settings
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
